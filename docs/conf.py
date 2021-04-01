f3b71ab25e9c67ec1ede4e73a0a87138c32fa3f9
Chris Sewell <chrisj_sewell@hotmail.com>
2021-04-01 09:37:42 +0200

    🔧 MAINTAIN: Initial repository structure

---
+++ docs/conf.py
@@ -0,0 +1,9 @@
+# Configuration file for the Sphinx documentation builder.
+
+project = "Sphinx External ToC"
+copyright = "2021, Executable Book Project"
+author = "Executable Book Project"
+
+master_doc = "index"
+
+extensions = ["myst_parser"]
---
+++ sphinx_external_toc/__init__.py
@@ -0,0 +1,3 @@
+"""A sphinx extension that allows the site toctree to be defined in a single file."""
+
+__version__ = "0.0.1"
