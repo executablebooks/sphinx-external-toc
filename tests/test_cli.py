085c7b032c293e00689549b4263fd52e53ccf29b
Chris Sewell <chrisj_sewell@hotmail.com>
2021-04-06 21:51:33 +0200

    Update test_cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -33,5 +33,6 @@ def test_parse_toc(invoke_cli):
     print(list(Path(__file__).parent.joinpath("_toc_files").glob("*")))
     print(Path(__file__).parent.joinpath("_toc_files", "basic.yml").exists())
     path = os.path.abspath(Path(__file__).parent.joinpath("_toc_files", "basic.yml"))
-    result = invoke_cli(parse_toc, path)
+    print(path)
+    result = invoke_cli(parse_toc, [path])
     assert "intro" in result.output
