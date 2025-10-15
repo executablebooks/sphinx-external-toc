398677306f6bb36e2a3f32dfa9efc1d8b33c4fd2
Dennis den Ouden-van der Horst <d.denouden-vanderhorst@tudelft.nl>
2025-10-15 15:22:20 +0200

    Update collectors.py

--- sphinx_external_toc/collectors.py
+++ sphinx_external_toc/collectors.py
@@ -65,6 +65,8 @@ class TocTreeCollectorWithStyles(TocTreeCollector):
                     new_secnumber = self.__renumber(env.titles[ref]["secnumber"],style)
                     logger.warning(f"[FORKED] New section number of {ref}: {new_secnumber}")
                     env.titles[ref]["secnumber"] = copy.deepcopy(new_secnumber)
+                    if ref in env.tocs:
+                        self.__replace_toc(env, ref, env.tocs[ref])
 
                     # STORE IN THE MAP
                     if isinstance(old_secnumber, list):
