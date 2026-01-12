45c0fe9bfd0e13a95361f842e3cfbf92f8f113d6
Tom van Woudenberg <t.r.vanwoudenberg@tudelft.nl>
2026-01-12 10:46:13 +0100

    formatting

--- sphinx_external_toc/__init__.py
+++ sphinx_external_toc/__init__.py
@@ -31,7 +31,7 @@ def setup(app: "Sphinx") -> dict:
     # variables
     app.add_config_value("external_toc_path", "_toc.yml", "env")
     app.add_config_value("external_toc_exclude_missing", False, "env")
-    
+
     # Register use_multitoc_numbering if not already registered (e.g., by JupyterBook)
     try:
         app.add_config_value("use_multitoc_numbering", True, "env")
